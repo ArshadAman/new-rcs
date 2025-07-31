287aaad5118cfae6ec59430470754edbb62d8a69
Arshad Aman <38555984+ArshadAman@users.noreply.github.com>
2025-08-01 02:38:06 +0530

    first commit

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    business_name = models.CharField(max_length=100, blank=True)
+    website_url = models.URLField(max_length=200, blank=True)
+    contact_number = models.CharField(max_length=20, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    PLAN_CHOICES = [
+        ('basic', 'Basic'),
+        ('standard', 'Standard'),
+        ('premium', 'Premium'),
+    ]
+    plan = models.CharField(max_length=10, choices=PLAN_CHOICES, default='basic')
+
+    def __str__(self):
+        return self.username
