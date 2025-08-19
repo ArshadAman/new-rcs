2c760769e257e56fbca73ca48658996750095787
Arshad Aman <38555984+ArshadAman@users.noreply.github.com>
2025-08-19 23:04:09 +0530

    migrations

--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,5 @@ git pull origin master
 echo "Rebuilding project..."
 docker-compose down
 docker-compose up -d --build
+docker-compose exec web python manage.py migrate
 echo "Project rebuilt successfully."
